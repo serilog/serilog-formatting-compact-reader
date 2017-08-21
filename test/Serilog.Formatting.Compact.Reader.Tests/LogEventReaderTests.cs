8c8ded664596d4ded642cb223676b4db9a3e51f1
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-08-22 09:35:55 +1000

    Fixes #15 - pass through unrecognized properties

--- test/Serilog.Formatting.Compact.Reader.Tests/LogEventReaderTests.cs
+++ test/Serilog.Formatting.Compact.Reader.Tests/LogEventReaderTests.cs
@@ -71,5 +71,18 @@ namespace Serilog.Formatting.Compact.Reader.Tests
             var user = (StructureValue)evt.Properties["User"];
             Assert.Equal("TestUser", user.TypeTag);
         }
+
+        [Fact]
+        public void PassesThroughUnrecognizedReifiedProperties()
+        {
+            const string document = "{\"@t\":\"2016-10-12T04:20:58.0554314Z\",\"@m\":\"Hello\",\"@foo\":42}";
+            var evt = LogEventReader.ReadFromString(document);
+
+            var foo = (ScalarValue)evt.Properties["@foo"];
+            Assert.Equal(42, (int)(decimal)foo.Value);
+
+            // Ensure we don't just forward everything
+            Assert.False(evt.Properties.ContainsKey("@m"));
+        }
     }
 }
